a8bcf4a704d7fcfe7ef95fd42c44dbcc58a50a97
Yamini <sureshvanithasureshvanitha50@gmail.com>
2026-08-30 20:37:44 +0530

    Update the 3rd TC test data

--- tests/WF_CALADN_03.spec.js
+++ tests/WF_CALADN_03.spec.js
@@ -8,12 +8,10 @@ const { InvoicePage } = require('../pages/InvoicePage');
 const { CalendarPage } = require('../pages/CalendarPage');
  
 const { patientData,appoinmentData,consultData,serviceData,DateData,invoiceData } = require('../testdata/TC_003.json');
-const { generateUniquePatientFullName } = require('../utils/RandomData');
  
 
 test('Generate Invoice', async ({ page }) => {
  
-    // const patientName = generateUniquePatientFullName();
     const patientName = patientData.patientName;
     const patientPage = new PatientPage(page);
     const consultPage = new ConsultPage(page);
@@ -21,11 +19,6 @@ test('Generate Invoice', async ({ page }) => {
     const invoicePage = new InvoicePage(page);
     const calendarPage = new CalendarPage(page);
  
-    // await patientPage.createPatient(
-    //     patientName,
-    //     patientData
-    // );//update
-
     const bookingDate =
     await consultPage.addConsult(
         patientName,
