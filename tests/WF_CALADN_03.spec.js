4290edfa27eb4b4848ddae4285d9df7c8e41da58
Yamini <sureshvanithasureshvanitha50@gmail.com>
2026-08-31 11:54:41 +0530

    Add a new patient in 3rd TC

--- tests/WF_CALADN_03.spec.js
+++ tests/WF_CALADN_03.spec.js
@@ -8,16 +8,22 @@ const { InvoicePage } = require('../pages/InvoicePage');
 const { CalendarPage } = require('../pages/CalendarPage');
  
 const { patientData,appoinmentData,consultData,serviceData,DateData,invoiceData } = require('../testdata/TC_003.json');
- 
+const { generateUniquePatientFullName } = require('../utils/RandomData');
 
 test('Generate Invoice', async ({ page }) => {
  
-    const patientName = patientData.patientName;
+    // const patientName = patientData.patientName;
+    const patientName = generateUniquePatientFullName();
     const patientPage = new PatientPage(page);
     const consultPage = new ConsultPage(page);
     const servicePage = new ServicePage(page);
     const invoicePage = new InvoicePage(page);
     const calendarPage = new CalendarPage(page);
+ 
+     await patientPage.createPatient(
+        patientName,
+        patientData
+    );
  
     const bookingDate =
     await consultPage.addConsult(
